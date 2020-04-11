df6f783b9aa134be207b663ffa35898c0d04328f
Guilherme <gbletsch@gmail.com>
2020-04-10 23:03:09 -0300

    Updates

---
+++ precache-manifest.95d68d7a1e56148f0b7e83057d60cd92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad306d2fd36ad092182db822c8b44000",
+    "url": "/gbletsch/clinic-management-system-frontend/index.html"
+  },
+  {
+    "revision": "e61e59c605aafd5d4297",
+    "url": "/gbletsch/clinic-management-system-frontend/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c1d73036f9bfd0beaccd",
+    "url": "/gbletsch/clinic-management-system-frontend/static/css/main.59a4d177.chunk.css"
+  },
+  {
+    "revision": "e61e59c605aafd5d4297",
+    "url": "/gbletsch/clinic-management-system-frontend/static/js/2.51ec71f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gbletsch/clinic-management-system-frontend/static/js/2.51ec71f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d73036f9bfd0beaccd",
+    "url": "/gbletsch/clinic-management-system-frontend/static/js/main.3f82ffd1.chunk.js"
+  },
+  {
+    "revision": "cfa6a1dec4288f3a7c71",
+    "url": "/gbletsch/clinic-management-system-frontend/static/js/runtime-main.ff7ce07f.js"
+  },
+  {
+    "revision": "8d953f4a5860fe313712b154fc1659a9",
+    "url": "/gbletsch/clinic-management-system-frontend/static/media/logo.8d953f4a.png"
+  }
+]);
